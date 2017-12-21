124058c944ed074bd5f2fc14dc319b47d0aeb1f0
Seth <s.zim517@gmail.com>
2017-12-20 19:26:50 -0500

    Adds animations to about page profile images

--- scripts/about.js
+++ scripts/about.js
@@ -1,42 +1,97 @@
-var translateBtn = document.querySelector('[data-toggle-translate]');
-var target = document.querySelector('[data-target]');
+var $sethTarget = $('[data-pic-seth]');
+var $alexTarget = $('[data-pic-alex]');
+var $evanTarget = $('[data-pic-evan]');
+var $andrewTarget = $('[data-pic-andrew]');
 
-var translateBtnTwo = document.querySelector('[data-toggle-translate-two]');
-var targetTwo = document.querySelector('[data-target-two]');
+    
+function sethPicOnCLick() {
+    $sethTarget.on('click', function(event) {
+            $sethTarget.toggleClass('translate1');
+            $alexTarget.toggleClass('animated fadeOutDown');
+            $evanTarget.toggleClass('animated fadeOutDown');
+            $andrewTarget.toggleClass('animated fadeOutDown');
+    })
+}
 
-var translateBtnThree = document.querySelector('[data-toggle-translate-three]');
-var targetThree = document.querySelector('[data-target-three]');
+function alexPicOnCLick() {
+    $alexTarget.on('click', function(event) {
+            $alexTarget.toggleClass('translate2');
+            $sethTarget.toggleClass('animated fadeOutDown');
+            $evanTarget.toggleClass('animated fadeOutDown');
+            $andrewTarget.toggleClass('animated fadeOutDown');
+    })
+}
 
-var translateBtnFour = document.querySelector('[data-toggle-translate-four]');
-var targetFour = document.querySelector('[data-target-four]');
+function evanPicOnCLick() {
+    $evanTarget.on('click', function(event) {
+            $evanTarget.toggleClass('translate3');
+            $sethTarget.toggleClass('animated fadeOutDown');
+            $alexTarget.toggleClass('animated fadeOutDown');
+            $andrewTarget.toggleClass('animated fadeOutDown');
+    })
+}
 
-translateBtn.addEventListener('click', function () {
-    target.classList.toggle('translate'); 
-    targetTwo.classList.remove('translate');
-    targetThree.classList.remove('translate');
-    targetFour.classList.remove('translate');
-});
+function andrewPicOnCLick() {
+    $andrewTarget.on('click', function(event) {
+            $andrewTarget.toggleClass('translate4');
+            $sethTarget.toggleClass('animated fadeOutDown');
+            $alexTarget.toggleClass('animated fadeOutDown');
+            $evanTarget.toggleClass('animated fadeOutDown');
+    })
+}
+
+sethPicOnCLick();
+alexPicOnCLick();
+evanPicOnCLick();
+andrewPicOnCLick();
 
 
-translateBtnTwo.addEventListener('click', function () {
-    targetTwo.classList.toggle('translate');
-    target.classList.remove('translate');
-    targetThree.classList.remove('translate');
-    targetFour.classList.remove('translate');
-});
 
 
-translateBtnThree.addEventListener('click', function () {
-    targetThree.classList.toggle('translate');
-    target.classList.remove('translate');
-    targetTwo.classList.remove('translate');
-    targetFour.classList.remove('translate');    
-});
+
+
+
+
+
+// var translateBtn = document.querySelector('[data-toggle-translate]');
+// var target = document.querySelector('[data-target]');
+
+// var translateBtnTwo = document.querySelector('[data-toggle-translate-two]');
+// var targetTwo = document.querySelector('[data-target-two]');
+
+// var translateBtnThree = document.querySelector('[data-toggle-translate-three]');
+// var targetThree = document.querySelector('[data-target-three]');
+
+// var translateBtnFour = document.querySelector('[data-toggle-translate-four]');
+// var targetFour = document.querySelector('[data-target-four]');
+
+// translateBtn.addEventListener('click', function () {
+//     target.classList.toggle('translate'); 
+//     targetTwo.classList.remove('translate');
+//     targetThree.classList.remove('translate');
+//     targetFour.classList.remove('translate');
+// });
+
+
+// translateBtnTwo.addEventListener('click', function () {
+//     targetTwo.classList.toggle('translate');
+//     target.classList.remove('translate');
+//     targetThree.classList.remove('translate');
+//     targetFour.classList.remove('translate');
+// });
+
+
+// translateBtnThree.addEventListener('click', function () {
+//     targetThree.classList.toggle('translate');
+//     target.classList.remove('translate');
+//     targetTwo.classList.remove('translate');
+//     targetFour.classList.remove('translate');    
+// });
 
    
-translateBtnFour.addEventListener('click', function () {
-    targetFour.classList.toggle('translate');
-    target.classList.remove('translate');
-    targetTwo.classList.remove('translate');
-    targetThree.classList.remove('translate');  
-});
+// translateBtnFour.addEventListener('click', function () {
+//     targetFour.classList.toggle('translate');
+//     target.classList.remove('translate');
+//     targetTwo.classList.remove('translate');
+//     targetThree.classList.remove('translate');  
+// });
